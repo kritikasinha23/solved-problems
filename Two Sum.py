9ca9ac2d0a68ed6dff4b198996ae79fdd52ad22c
kritikasinha23 <100464424+kritikasinha23@users.noreply.github.com>
2022-04-28 21:31:44 +0530

    Update Two Sum.py
    Added a comment

--- Two Sum.py
+++ Two Sum.py
@@ -1,8 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dct = {}
-        for index,value in enumerate(nums):
+        dct = {}  # creating an empty dictionary 
+        for index,value in enumerate(nums):  
             indices = target - nums[index]
             if indices in dct:
                 return (dct[indices], index)
             dct[value] = index
+            
